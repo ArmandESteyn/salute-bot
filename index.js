7bc5525facdabe8aa138113990ef0cb792850852
Unknown <ArmandS@fivefriday.com>
2017-02-17 09:48:40 +0200

    Authentication

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ server.get('/test/:name', function (req, res) {
 
 
 //Setup Bot
-var connector = new builder.ChatConnector();
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 bot.dialog('/', intents);
