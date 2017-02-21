2448d56215ec5514a888390cf575828dec491a94
Unknown <ArmandS@fivefriday.com>
2017-02-21 16:31:07 +0200

    password

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 //Setup Bot
 var connector = new builder.ChatConnector({
     appId:process.env.MICROSOFT_APP_ID,
-    appPassword:process.env.MICROSOFT_APP_ID
+    appPassword:process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
